28aa73fca31394afd75e26a03243c2fc161ec145
rizza <denostarizza43@gmail.com>
2023-05-12 03:09:05 +1200

    Pseudocode

---
+++ app calculator.py
@@ -0,0 +1,6 @@
+# Print the title of program
+# Ask the user for there desired operation
+# Ask user to enter the numbers to be calculated
+# Perform the selected operation and print the result of calculation
+# Ask if the user wants to try again
+# Print the output
