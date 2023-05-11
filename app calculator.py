c2231171b3c650c0a341695a9dffddedfc1430b8
rizza <denostarizza43@gmail.com>
2023-05-12 03:32:51 +1200

    Ask user to choose a operation

--- app calculator.py
+++ app calculator.py
@@ -5,7 +5,30 @@ print(title)
 repeat = True
 
 while repeat:
-# Ask the user for there desired operation
+    # Ask the user for there desired operation
+    print("Choose an operation:")
+    add = ("1 Addition")
+    print(add)
+
+    sub = ("2 Subtraction")
+    print(sub)
+
+    mul = ("3 Multiplication")
+    print(mul)
+
+    div = ("4 Division")
+    print(div)
+
+    try:
+        user_choice = int(input("Enter your choosen operation (1-4): "))
+        if user_choice not in [1, 2, 3, 4]:
+            print("Invalid choice. Please enter a valid choice.")
+            continue
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
+
+
 # Ask user to enter the numbers to be calculated
 # Perform the selected operation and print the result of calculation
 # Ask if the user wants to try again
