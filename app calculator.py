daed780b82933c9691b89711724387aab214b313
rizza <denostarizza43@gmail.com>
2023-05-12 03:16:01 +1200

    while loop added

--- app calculator.py
+++ app calculator.py
@@ -2,6 +2,9 @@
 title = "SIMPLE APP CALCULATOR"
 print(title)
 
+repeat = True
+
+while repeat:
 # Ask the user for there desired operation
 # Ask user to enter the numbers to be calculated
 # Perform the selected operation and print the result of calculation
